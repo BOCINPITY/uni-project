4f566999578c3a95feb3cd35e74f5e567a8e30b8
qinjian <208023732@qq.com>
2024-09-19 15:53:18 +0800

    tabbar配置

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,12 +1,18 @@
 <template>
-	<div>
-
-	</div>
+	<view>
+		<view class="container">
+			<text>left</text>
+			<text>middle</text>
+			<text>right</text>
+		</view>
+	</view>
 </template>
 
 <script setup>
 </script>
 
 <style lang="scss" scoped>
-
+.container{
+	@include flex();
+}
 </style>
